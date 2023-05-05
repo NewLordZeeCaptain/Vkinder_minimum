2bf12766d9df6d67203284bda0850159adee8fb3
newlordzeecaptain <lordzeecaptain@ya.ru>
2023-05-05 14:21:19 +0300

    tokens

--- Config/config.py
+++ Config/config.py
@@ -1,5 +1,5 @@
-user_token = "vk1.a.6Vqzv5bqO6AGZyDcFTmNs5LEbDyaJDbGQDZursO106U4GOOkkfzMb4dz4fL18hQKbDkH38QN6-7gJrUWSsfvqcOjeoXzFcgLtgCNVAwAubeEPEk3Sy4KOnZli1Lb-IJatB384HBpyhDW4z_jTUdvB5ZW-Q4wOyziiz3gA28FxTXJ78EUgxRVLbbtxUuMyqkU0oCPBYyZ1CbFyVI8NzwyOA"  # токен пользователя
-comm_token = "vk1.a.LpYhPSVZqI3WC0Rt1WichfZ1YLaoU1xyyWJ56N71hubb7gFHpJuVDV40nXx7UhU7Jhapht7c0xS7b7jTKciBiOduTayRaPfzLh9ErnQuHY5juuY53tVFXUSTz5rSbMf2mbzi3wKqZJ-m70YYE-QKd_RgDWiWNvYJSoNZyyUf_l0nfkWdelLmuoPvfXgGpm8AnFLPhk3PrnpUmRy4h7XbbQ"  # токен сообщества
+user_token = ""  # токен пользователя
+comm_token = ""  # токен сообщества
 
 offset = 0  # сдвиг
 line = range(0, 1000)  # последовательность для перебора найденных пользователей
